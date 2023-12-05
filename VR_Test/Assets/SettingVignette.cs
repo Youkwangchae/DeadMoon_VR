5f7c26036a618e95e1610f9b21dfa6ed4191fcd2
Youkwangchae <gc9612@naver.com>
2023-12-05 19:20:43 +0900

    [Fix] Final Fix & Test

--- VR_Test/Assets/SettingVignette.cs
+++ VR_Test/Assets/SettingVignette.cs
@@ -11,20 +11,48 @@ public class SettingVignette : MonoBehaviour
     UnityEngine.Rendering.Universal.Vignette vignette;
     float delta;
     float duration = 20.0f;
+    private bool isFirst = true;
     // Start is called before the first frame update
     void Start()
     {
-        volume = GetComponent<Volume>();
+        if (volume == null)
+            volume = GetComponent<Volume>();
+
+        GameObject.Find("AroundTrigger").GetComponent<AroundZombie>().enabled = false;
+
+        vignette.color.value = Color.black;
     }
+
+    //IEnumerator vignetteSetPro()
+    //{
+    //    vignette.color.value = Color.black;
+    //    vignette.intensity.value = 0.3f;
+    //    yield return null;
+
+    //    while(vignette.intensity.value < 1)
+    //    {
+    //        float time = delta / duration;
+
+    //        vignette.intensity.value += ((1 - vignette.intensity.value) / 2) * (time);
+
+    //        delta += Time.deltaTime;
+
+    //        yield return null;
+    //    }
+
+    //    vignette.intensity.value = 1;
+    //}
 
     // Update is called once per frame
     void Update()
-    {
-        if(volume.profile.TryGet(out vignette))
+    {        
+        if (volume.profile.TryGet(out vignette))
         {
+            vignette.color.value = Color.black;
+
             float time = delta / duration;
 
-            vignette.intensity.value += ((1 - vignette.intensity.value)/2) * (time);
+            vignette.intensity.value += ((1 - vignette.intensity.value) / 2) * (time);
 
             delta += Time.deltaTime;
         }
